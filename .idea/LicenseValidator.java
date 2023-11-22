b1296739e1f023372c70c3b5dec49d0eb7aade87
Tomás Paixão Rosa <tomasrosa@ua.pt>
2023-11-22 13:55:55 +0000

    esqueleto
    Esqueleto do licenseValidator

---
+++ .idea/LicenseValidator.java
@@ -0,0 +1,27 @@
+public class LicenseValidator {
+
+    private String appName;
+    private String version;
+
+
+    public LicenseValidator(String nomeDaApp, String versao) {
+        this.appName = nomeDaApp;
+        this.version = versao;
+    }
+
+    public boolean isRegistered() {
+
+    }
+
+
+    public boolean startRegistration() {
+
+    }
+
+
+    public void showLicenseInfo() {
+
+    }
+
+
+}
