bc0077a69db23f43f1029531d062d98db991960d
João Filipe Fernandes Coelho <draganoid5@live.com>
2023-11-23 18:01:56 +0000

    config cc

--- Autenticacao/src/UserDataCollector.java
+++ Autenticacao/src/UserDataCollector.java
@@ -1,9 +1,40 @@
 import java.util.Scanner;
+import pt.gov.cartaodecidadao.*; // Esta é uma importação genérica para o SDK do Cartão de Cidadão
 
 public class UserDataCollector {
-    Scanner sc = new Scanner(System.in);
+    private Scanner sc = new Scanner(System.in);
+    private PTEID_ReaderSet readerSet;
+    private PTEID_EIDCard card;
+    private PTEID_EId eid;
 
-    public void getUserInput(){
+    public UserDataCollector() {
+        // Inicialize o leitor do Cartão de Cidadão aqui
+        // Certifique-se de que o SDK do Cartão de Cidadão está configurado corretamente
+    }
 
+    public UserData getUserInput() {
+        UserData userData = new UserData();
+
+        // Coletar e-mail do usuário
+        System.out.println("Digite o seu e-mail:");
+        userData.setEmail(sc.nextLine());
+
+        // Coletar dados do Cartão de Cidadão
+        try {
+            readerSet = PTEID_ReaderSet.instance();
+            card = readerSet.getReader().getEIDCard();
+            eid = card.getID();
+
+            userData.setName(eid.getGivenName() + " " + eid.getSurname());
+            userData.setId(eid.getDocumentNumber());
+            // Adicione aqui mais campos conforme necessário
+
+        } catch (PTEID_Exception e) {
+            e.printStackTrace();
+            // Tratamento de erros relacionados à leitura do Cartão de Cidadão
+        }
+
+        return userData;
     }
 }
+
